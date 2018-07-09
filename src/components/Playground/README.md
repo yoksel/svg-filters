0bcfe2375f14188246a00068dbb43b369c210a8b
yokselzok <yokselzok@gmail.com>
2018-07-09 23:05:20 +0300

    Add proptypes & doc for Playground

--- src/components/Playground/README.md
+++ src/components/Playground/README.md
@@ -1,1 +1,7 @@
 # Playground
+
+Demo-content for testing filters. Contains image, shapes & text.
+
+## Props
+
+`filterId` — ID of filter if filter exists.
--- src/components/Playground/index.js
+++ src/components/Playground/index.js
@@ -1,40 +1,47 @@
 import React, {Component} from 'react';
 import './Playground.css';
+import PropTypes from 'prop-types';
 
 import Filter from '../../containers/Filter';
 
-class Playground extends Component {
-  render() {
-    const filterId = this.props.filterId;
-    const filterUrl = filterId ? `url(#${filterId})` : '';
+const Playground = ({filterId}) => {
+  const filterUrl = filterId ? `url(#${filterId})` : '';
 
-    return (
-      <div className="Playground">
-        <div className="Playground__image">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            width="250" height="320">
+  return (
+    <div className="Playground">
+      <div className="Playground__image">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          className="Playground__svg"
+          width="250" height="320">
 
-            <Filter/>
+          <Filter/>
 
-            <g filter={filterUrl}>
-              <image xlinkHref="http://placekitten.com/250/150"/>
+          <image
+            xlinkHref="http://placekitten.com/250/150"
+            filter={filterUrl}
+          />
 
-              <rect x="50%" y="50%" width="50%" height="100" fill="teal"/>
+          <g filter={filterUrl}>
+            <rect x="50%" y="50%" width="50%" height="100" fill="teal"/>
 
-              <circle r="70" cx="40%" cy="180" fill="gold" stroke="yellowgreen" strokeWidth="5"/>
-              <text
-                x="50%" y="260" dy="1em"
-                textAnchor="middle">Some text</text>
-            </g>
-          </svg>
-        </div>
-
-        (code)
+            <circle r="70" cx="40%" cy="180" fill="gold" stroke="yellowgreen" strokeWidth="5"
+            />
+            <text
+              x="50%" y="260" dy="1em"
+              textAnchor="middle">Some text</text>
+          </g>
+        </svg>
       </div>
-    );
-  }
-}
+
+      (code)
+    </div>
+  );
+};
 
 export default Playground;
+
+Playground.propTypes = {
+  filterId: PropTypes.string
+};
