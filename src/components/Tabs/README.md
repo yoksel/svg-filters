559a2f24cce7e12e8ee4c98c7bfe930c9b17611a
yokselzok <yokselzok@gmail.com>
2018-07-12 19:29:51 +0300

    Add component Tabs

---
+++ src/components/Tabs/README.md
@@ -0,0 +1,12 @@
+# Tabs
+
+Component for panels switching by tabs.
+
+## Prop
+
+`items` — list of objects like this:
+
+```{
+  name: 'Name for tab'
+  content: <Component/>
+}```
---
+++ src/components/Tabs/index.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import './Tabs.css';
+
+class Tabs extends Component {
+  state = {
+    active: this.props.items[0].id
+  };
+
+  setActive = (tabName) => {
+    this.setState({
+      active: tabName
+    });
+  };
+
+  render() {
+    console.log(this.props);
+    const {items} = this.props;
+    const tabsList = items.reduce((prev, item) => {
+      const id = item.id;
+
+      let buttonClass = 'Tabs__control';
+      if (id === this.state.active) {
+        buttonClass += ` ${buttonClass}--active`;
+      }
+
+      const button = <button
+        key={id}
+        className={buttonClass}
+        onClick={() => {
+          this.setActive(id);
+        }}
+      >
+        <span className="Tabs__control-text">
+          {id}
+        </span>
+      </button>;
+
+      let contentClass = 'Tabs__item';
+      if (id === this.state.active) {
+        contentClass += ` ${contentClass}--active`;
+      }
+      const content = <div
+        key={id}
+        className={contentClass}
+      ><item.content/></div>;
+
+      prev.controls.push(button);
+      prev.tabs.push(content);
+
+      return prev;
+    },{
+      controls: [],
+      tabs: []
+    });
+
+    return (
+      <div className="Tabs">
+        <div className="Tabs__controls">
+          {tabsList.controls}
+        </div>
+        {tabsList.tabs}
+      </div>
+    );
+  }
+}
+
+export default Tabs;
+
+Tabs.propTypes = {
+  items: PropTypes.array
+};
