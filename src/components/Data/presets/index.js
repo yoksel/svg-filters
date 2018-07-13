3da699326c6394ec05efa6b3c86750c9849cbbf2
yokselzok <yokselzok@gmail.com>
2018-07-13 13:30:10 +0300

    Add preset `ColoredSpots`

--- src/components/Data/presets/index.js
+++ src/components/Data/presets/index.js
@@ -2,10 +2,12 @@ import blur from './blur';
 import grayscale from './grayscale';
 import smoke from './smoke';
 import waves from './waves';
+import coloredSpots from './coloredSpots';
 
 export default [
   blur,
   grayscale,
   waves,
   smoke,
+  coloredSpots
 ];
