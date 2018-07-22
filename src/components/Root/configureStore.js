0d3314517e8d267df6aac06c404066e76dfeab95
yokselzok <yokselzok@gmail.com>
2018-07-22 14:17:50 +0300

    Add actions logging for dev env

--- src/components/Root/configureStore.js
+++ src/components/Root/configureStore.js
@@ -1,8 +1,31 @@
 import {createStore} from 'redux';
 import reducers from '../../store/reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.groupCollapsed(action.type);
+    console.log('Before:', store.getState());
+    console.log('Action:', action);
+    const result = rawDispatch(action);
+    console.log('After:', store.getState());
+    console.groupEnd(action.type);
+
+    return result;
+  };
+};
+
 const configureStore = () => {
   const store = createStore(reducers);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   return store;
 };
