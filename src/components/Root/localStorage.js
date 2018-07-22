3e7276742ca2400d1c83791ad80a22466dba58dd
yokselzok <yokselzok@gmail.com>
2018-07-22 17:00:49 +0300

    Add forgotten localStorage

---
+++ src/components/Root/localStorage.js
@@ -0,0 +1,13 @@
+export const saveState = (state) => {
+  localStorage.setItem('sf-state', JSON.stringify(state));
+};
+
+export const getState = () => {
+  const storageValue = localStorage.getItem('sf-state');
+
+  if (storageValue) {
+    return JSON.parse(storageValue);
+  }
+
+  return undefined;
+};
