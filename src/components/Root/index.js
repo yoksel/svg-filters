c4a284c782b41a03a7e194574fdc42d1c54e32db
yokselzok <yokselzok@gmail.com>
2018-07-26 21:48:38 +0300

    Replace component App with container

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-import App from '../App';
+import App from '../../containers/App';
 import configureStore from './configureStore';
 
 const store = configureStore();
