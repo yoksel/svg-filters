e2cd6f65b63c39581fca7804f03f7c15308c505e
yokselzok <yokselzok@gmail.com>
2018-07-30 19:17:19 +0300

    Remove dragging logic from Constructor; add DragDrop & DragDropItem to it

--- src/components/Constructor/index.js
+++ src/components/Constructor/index.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 
 import deepClone from '../../helpers/deepClone';
 
-import Placeholder from '../Placeholder';
 import PrimitivePanel from '../../components/PrimitivePanel';
+import DragDropItem from '../../containers/DragDropItem';
+import DragDrop from '../../containers/DragDrop';
 
 import './Constructor.css';
 
@@ -18,108 +19,67 @@ const Contsructor = ({primitives, dragDrop}) => {
     main: []
   };
 
-  const getSiblingsCoords = ({id, parentId = 'main'}) => {
-    const list = items[parentId].filter(item => item);
-
-    const itemsCoords = list.map((item, index) => {
-      const {top, bottom} = item.getBoundingClientRect();
-      const itemId = item.id;
-
-      return {
-        id: itemId,
-        index,
-        top,
-        bottom
-      };
-    });
-    return itemsCoords;
-  };
-
-  const getPosition = (id) => {
-    const isDragging = dragDrop && id === dragDrop.id;
-    let position = {};
-
-    if (isDragging) {
-      position = {
-        ...dragDrop.coords,
-        position: 'fixed',
-        zIndex: 10,
-        width: dragDrop.elemClientRect.width
-      };
-    }
-
-    return position;
-  };
-
   return (
     <div className="Contsructor">
       &lt;filter id="#filter">
-      <div className="Contsructor__container">
-        {primitives.map((primitive, index) => {
-          const isDragging = dragDrop && primitive.id === dragDrop.id;
-          let position = getPosition(primitive.id);
+      <div
+        className="Contsructor__container">
+        <DragDrop
+          listId="main">
+          {primitives.map((primitive, index) => {
 
-          if (primitive.children && primitive.children.length > 0) {
-            primitive = deepClone(primitive);
-            items[primitive.id] = [];
+            if (primitive.children && primitive.children.length > 0) {
+              primitive = deepClone(primitive);
+              items[primitive.id] = [];
 
-            primitive.children = primitive.children.map((item, childIndex) => {
-              let childPosition = getPosition(item.id);
-              const isChildDragging = item.id === dragDrop.id;
-
-              return (
-                <div
-                  key={item.id}
-                  id={item.id}
-                  className="Contsructor__item"
-                  ref={node => items[primitive.id].push(node)}
-                >
+              primitive.children = primitive.children.map((item, childIndex) => {
+                return (
                   <div
-                    className="Contsructor__item-container"
-                    style={childPosition}
+                    key={item.id}
+                    id={item.id}
+                    className="Contsructor__item"
+                    ref={node => items[primitive.id].push(node)}
                   >
-                    <PrimitivePanel
-                      index={childIndex}
+                    <DragDropItem
+                      id={item.id}
                       parentId={primitive.id}
-                      primitive={item}
-                      resultsList={getResultsList(primitives, index)}
-                      getSiblingsCoords={getSiblingsCoords}
-                    />
+                      listId={primitive.id}
+                      index={childIndex}
+                    >
+                      <PrimitivePanel
+                        index={childIndex}
+                        parentId={primitive.id}
+                        primitive={item}
+                        resultsList={getResultsList(primitives, index)}
+                      />
+                    </DragDropItem>
                   </div>
+                );
+              });
+            }
 
-                  <Placeholder
-                    isDragging={isChildDragging}
-                    elemClientRect={dragDrop.elemClientRect}/>
-                </div>
-              );
-            });
-          }
-
-          return (
-            <div
-              key={primitive.id}
-              id={primitive.id}
-              className="Contsructor__item"
-              ref={node => items.main.push(node)}
-            >
+            return (
               <div
-                className="Contsructor__item-container"
-                style={position}
+                key={primitive.id}
+                id={primitive.id}
+                className="Contsructor__item"
+                ref={node => items.main.push(node)}
               >
-                <PrimitivePanel
+                <DragDropItem
+                  id={primitive.id}
+                  listId="primitives"
                   index={index}
-                  primitive={primitive}
-                  resultsList={getResultsList(primitives, index)}
-                  getSiblingsCoords={getSiblingsCoords}
-                />
+                >
+                  <PrimitivePanel
+                    index={index}
+                    primitive={primitive}
+                    resultsList={getResultsList(primitives, index)}
+                  />
+                </DragDropItem>
               </div>
-
-              <Placeholder
-                isDragging={isDragging}
-                elemClientRect={dragDrop.elemClientRect}/>
-            </div>
-          );
-        })}
+            );
+          })}
+        </DragDrop>
       </div>
 
       &lt;/filter>
