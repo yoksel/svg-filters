d4c6a38239507d0e1a8db00b93a65d2b74494b1d
yokselzok <yokselzok@gmail.com>
2018-07-30 19:14:46 +0300

    Remove DragDrop from Root

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
 import App from '../../containers/App';
-import DragDrop from '../../containers/DragDrop';
 import configureStore from './configureStore';
 
 const store = configureStore();
@@ -11,14 +10,12 @@ const store = configureStore();
 const Root = () => {
   return (
     <Provider store={store}>
-      <DragDrop>
-        <Router>
-          <Route
-            path="/:sidebarList?/:presetId?"
-            component={App}
-          />
-        </Router>
-      </DragDrop>
+      <Router>
+        <Route
+          path="/:sidebarList?/:presetId?"
+          component={App}
+        />
+      </Router>
     </Provider>
   );
 };
