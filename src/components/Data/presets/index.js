bc74821c4a22b4b218379c784d42c1fca8484fe3
yokselzok <yokselzok@gmail.com>
2018-07-31 13:10:30 +0300

    Put back shadow

--- src/components/Data/presets/index.js
+++ src/components/Data/presets/index.js
@@ -1,6 +1,7 @@
 import blur from './blur';
 import grayscale from './grayscale';
 import smoke from './smoke';
+import shadow from './shadow';
 import waves from './waves';
 import coloredSpots from './coloredSpots';
 import coloredStripesHor from './coloredStripesHor';
@@ -8,6 +9,7 @@ import coloredStripesVert from './coloredStripesVert';
 
 export default [
   blur,
+  shadow,
   grayscale,
   waves,
   smoke,
