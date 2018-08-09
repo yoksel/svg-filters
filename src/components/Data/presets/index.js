9c02dda5bd7070e8f5e804f1d5917fb982167d9f
yokselzok <yokselzok@gmail.com>
2018-08-09 14:11:05 +0300

    Add stroke, dancingStroke & contour presets

--- src/components/Data/presets/index.js
+++ src/components/Data/presets/index.js
@@ -1,6 +1,9 @@
 import blur from './blur';
 import grayscale from './grayscale';
 import smoke from './smoke';
+import stroke from './stroke';
+import dancingStroke from './dancingStroke';
+import contour from './contour';
 import zebra from './zebra';
 import shadow from './shadow';
 import waves from './waves';
@@ -11,6 +14,9 @@ import coloredStripesVert from './coloredStripesVert';
 export default [
   blur,
   shadow,
+  stroke,
+  dancingStroke,
+  contour,
   grayscale,
   waves,
   smoke,
