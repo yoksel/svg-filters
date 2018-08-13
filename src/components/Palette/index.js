37f272d84ae3c680fc60b1138ede1f788c9f7179
yokselzok <yokselzok@gmail.com>
2018-08-14 02:50:55 +0300

    Remove text colors

--- src/components/Palette/index.js
+++ src/components/Palette/index.js
@@ -7,17 +7,11 @@ class Palette extends Component {
     return (
       <div className="Palette">
         <ul>
-          <li className="hlt hlt--red">hlt--red</li>
-          <li className="hlt hlt--orange">hlt--orange</li>
-          <li className="hlt hlt--gold">hlt--gold</li>
-          <li className="hlt hlt--green">hlt--green</li>
-          <li className="hlt hlt--blue">hlgt--blue</li>
-
-          <li className="color-text color-text--red">red</li>
-          <li className="color-text color-text--orange">orange</li>
-          <li className="color-text color-text--gold">gold</li>
-          <li className="color-text color-text--green">green</li>
-          <li className="color-text color-text--blue">blue</li>
+          <li className="hlt hlt--red">hlt-red</li>
+          <li className="hlt hlt--orange">hlt-orange</li>
+          <li className="hlt hlt--gold">hlt-gold</li>
+          <li className="hlt hlt--green">hlt-green</li>
+          <li className="hlt hlt--blue">hlt-blue</li>
         </ul>
       </div>
     );
