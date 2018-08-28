d44d2e6fbd01b33d7078b9bd36c3cc0c5213a6fc
yokselzok <yokselzok@gmail.com>
2018-08-28 23:21:23 +0300

    Add new presets

--- src/components/Data/presets/index.js
+++ src/components/Data/presets/index.js
@@ -8,10 +8,14 @@ import stroke from './stroke';
 import dancingStroke from './dancingStroke';
 import contour from './contour';
 import zebra from './zebra';
+import net from './net';
+import layeredNet from './layeredNet';
+import dust from './dust';
 import shadow from './shadow';
 import innerShadow from './innerShadow';
 import waves from './waves';
 import coloredSpots from './coloredSpots';
+import coloredSpots2 from './coloredSpots2';
 import coloredStripesVert from './coloredStripesVert';
 import coloredFlame from './coloredFlame';
 
@@ -29,7 +33,11 @@ export default [
   waterDrops,
   paintDrops,
   zebra,
+  net,
+  layeredNet,
+  dust,
   coloredStripesVert,
   coloredSpots,
+  coloredSpots2,
   coloredFlame,
 ];
