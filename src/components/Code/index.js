e6327a2bae7272df51ed229cdd61503ffab0e9e3
yokselzok <yokselzok@gmail.com>
2018-09-01 19:14:47 +0300

    Add docs for Code components

--- src/components/Code/index.js
+++ src/components/Code/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import deepClone from '../../helpers/deepClone';
 
@@ -126,3 +127,8 @@ class Code extends Component {
 }
 
 export default Code;
+
+Code.propTypes = {
+  filterData: PropTypes.object,
+  primitives: PropTypes.array
+};
--- src/containers/Code/README.md
+++ src/containers/Code/README.md
@@ -1,1 +1,5 @@
 # Code
+
+Gets primitives & filterData from state, passes them to template, which outputs filter code to page.
+
+__Template:__ [Code](#code)
