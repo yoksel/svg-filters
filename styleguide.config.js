3aea86ee89fae7e3d2dc8db1ac1f2d5d03a194a1
yokselzok <yokselzok@gmail.com>
2018-09-01 22:09:09 +0300

    Update styleguide.config.js

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,15 +1,22 @@
 module.exports = {
   // components: 'src/components/**/index.js',
+  ignore: [
+    '**/Data/docs/*',
+    '**/Data/presets/*',
+    '**/Data/primitives/*',
+    '**/Data/primitivesAttrs/*'],
   sections: [
     {
       name: 'Components',
       description: 'Visual components',
-      components: 'src/components/**/index.js'
+      components: 'src/components/**/index.js',
+      usageMode: 'expanded'
     },
     {
       name: 'Containers',
       description: 'Components, connected to store',
-      components: 'src/containers/**/index.js'
+      components: 'src/containers/**/index.js',
+      usageMode: 'expanded'
     }
   ],
   styles: {
