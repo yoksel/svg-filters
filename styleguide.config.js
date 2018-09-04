2b1a37642bb9a17eebec5abc0cc925d719c3a7e2
yokselzok <yokselzok@gmail.com>
2018-09-04 19:30:09 +0300

    Update styleguide settings

--- styleguide.config.js
+++ styleguide.config.js
@@ -8,17 +8,20 @@ module.exports = {
   sections: [
     {
       name: 'Components',
-      description: 'Visual components',
+      description: '<hr/><h3>Visual components</h3><hr/>',
       components: 'src/components/**/index.js',
       usageMode: 'expanded'
     },
     {
       name: 'Containers',
-      description: 'Components, connected to store',
+      description: '<hr/><h3>Components, connected to store</h3><hr/>',
       components: 'src/containers/**/index.js',
       usageMode: 'expanded'
     }
   ],
+  theme: {
+    sidebarWidth: 260,
+  },
   styles: {
     Heading: {
       heading1: {
