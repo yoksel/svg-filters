372d82f95f81e0d72b3c41eccdea478cacb5cfbb
yoksel <juliahtml@gmail.com>
2024-10-14 18:22:58 +0100

    Get rid of UI component Code as it uses containers

--- src/components/Code/README.md
+++
@@ -1,7 +0,0 @@
-# Code
-
-Prints filter code to textarea.
-
-If ```process.env.NODE_ENV !== 'production'```, outputs current filter JSON to console. It's useful for grabbing current filter primitives to preset.
-
-__Container:__ [Code](#code-1)
--- src/containers/Code/index.tsx
+++ src/containers/Code/index.tsx
@@ -1,17 +1,79 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
-import Code from '../../components/Code';
 import useSection from '../../hooks/useSection';
-import { isPrimitivesSection } from '../../store/types';
+import { FilterParams, isPrimitivesSection, PrimitiveItem } from '../../store/types';
+import ColorInterpolFiltersSwitcher from '../ColorInterpolFiltersSwitcher';
+import { getAllPrimitivesCode } from './helpers';
 
-const CodeContainer = () => {
+import './Code.scss';
+
+const getFilterAttrs = (filterData?: FilterParams) => {
+  if (!filterData) return;
+
+  if (filterData.style) {
+    // Do not print style in filter code
+    delete filterData.style;
+  }
+
+  const attrsList = Object.keys(filterData).reduce<string[]>((prev, attrName) => {
+    let value = filterData[attrName as keyof FilterParams];
+    let valueAsString: string = '';
+    if (typeof value === 'object') {
+      const valueWithStringTypes = value as { [key: string]: string };
+      const valueList = Object.keys(valueWithStringTypes).reduce<string[]>((prev, valueKey) => {
+        prev.push(`${valueKey}: ${valueWithStringTypes[valueKey]}`);
+
+        return prev;
+      }, []);
+
+      valueAsString = valueList.join(';');
+    }
+    if (attrName === 'colorInterpolationFilters') {
+      attrName = 'color-interpolation-filters';
+    }
+
+    prev.push(`${attrName}="${valueAsString || value}"`);
+
+    return prev;
+  }, []);
+
+  return attrsList.join(' ');
+};
+
+const printCode = (primitives: PrimitiveItem[]) => {
+  console.groupCollapsed('Filter code');
+  console.log(JSON.stringify(primitives, null, '\t'));
+  console.groupEnd();
+};
+
+/** Section containing Textarea with filter code and color interpolation switcher */
+const Code = () => {
   const { section } = useSection();
   const primitivesFilter = useSelector((state: RootState) => state.primitives.filter);
   const primitivesBySections = useSelector((state: RootState) => state.primitives.sections);
 
   if (!isPrimitivesSection(section)) return null;
 
-  return <Code primitives={primitivesBySections[section]} filterData={primitivesFilter} />;
+  const primitives = primitivesBySections[section];
+  const filterAttrs = getFilterAttrs(primitivesFilter);
+  const primitivesCode = getAllPrimitivesCode(primitives).join('\n');
+
+  if (process.env.NODE_ENV !== 'production' && primitives?.length) {
+    // Temporary for extracting good presets
+    // printCode()
+  }
+
+  const value = primitivesCode
+    ? `<filter id="filter" ${filterAttrs}>\n${primitivesCode}\n</filter>`
+    : '';
+
+  return (
+    <section className="Code">
+      <h2 className="visually-hidden">Filter code</h2>
+      <ColorInterpolFiltersSwitcher />
+      <textarea className="Code__textarea" value={value} spellCheck="false" />
+    </section>
+  );
 };
 
-export default CodeContainer;
+export default Code;
