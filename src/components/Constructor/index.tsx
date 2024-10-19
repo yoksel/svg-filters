2a2d4ed4a30c40a5736796579d3c1ab603034cdd
yoksel <juliahtml@gmail.com>
2024-10-19 13:47:28 +0100

    Contructor component refactoring

--- src/components/Constructor/index.tsx
+++ src/components/Constructor/index.tsx
@@ -1,42 +1,9 @@
 import { NavLink } from 'react-router-dom';
-import { primitivesAttrs } from '../../data';
-
-import PrimitivePanel from '../../containers/PrimitivePanel';
-import ConstructorPlaceholder from '../atoms/ConstructorPlaceholder';
 import { PrimitiveItem, Section } from '../../store/types';
-import { defaultSources } from '../../store/reducers/helpers/updateInPropInPrimitiveItem';
-import ConstructorItem from '../../containers/ConstructorItem';
 
 import './Constructor.scss';
 
-const getResultsList = (primitives: PrimitiveItem[], index: number) => {
-  const results = primitives.slice(0, index).map((item) => item.id);
-  return Object.keys(defaultSources).concat(results);
-};
-
-const getMoveToPlaygroundButton = (
-  section: Section,
-  moveToPlayground: () => void,
-  count?: number,
-) => {
-  if (section === 'playground' || count === 0) {
-    return null;
-  }
-
-  return (
-    <span>
-      <NavLink
-        className="Constructor__button Constructor__button--move"
-        to="/"
-        onClick={moveToPlayground}
-      >
-        Move to playground
-      </NavLink>
-    </span>
-  );
-};
-
-interface ConstructorProps {
+interface ConstructorProps extends React.PropsWithChildren {
   primitives?: PrimitiveItem[];
   purgePrimitives: () => void;
   moveToPlayground: () => void;
@@ -48,13 +15,19 @@ const Constructor = ({
   purgePrimitives,
   moveToPlayground,
   section,
+  children,
 }: ConstructorProps) => {
+  if (!primitives?.length) return null;
+
+  const showMoveToPlaygroundButton = section !== 'playground' && primitives.length > 0;
+  const showClearButton = Boolean(primitives?.length) && section === 'playground';
+
   return (
     <section className="Constructor">
       <h2 className="visually-hidden">Constructor</h2>
-      <div className="Constructor__tag Constructor__tag--open">
+      <div className="Constructor__tag">
         &lt;filter id="#filter"&gt;
-        {Boolean(primitives?.length) && section === 'playground' && (
+        {showClearButton && (
           <button
             className="Constructor__button Constructor__button--purge"
             onClick={purgePrimitives}
@@ -62,51 +35,19 @@ const Constructor = ({
             Clear
           </button>
         )}
-        {getMoveToPlaygroundButton(section, moveToPlayground, primitives?.length)}
+        {showMoveToPlaygroundButton && (
+          <NavLink
+            className="Constructor__button Constructor__button--move"
+            to="/"
+            onClick={moveToPlayground}
+          >
+            Move to playground
+          </NavLink>
+        )}
       </div>
-      <div className="Constructor__container">
-        {!primitives?.length && <ConstructorPlaceholder section={section} />}
-        {primitives?.map((primitive: PrimitiveItem, index: number) => {
-          const groupData = primitivesAttrs[primitive.groupName];
+      <div className="Constructor__container">{children}</div>
 
-          return (
-            <ConstructorItem
-              key={primitive.id}
-              id={primitive.id}
-              listId="primitives"
-              index={index}
-              justAdded={primitive.justAdded}
-              nativeEvent={primitive.nativeEvent || undefined}
-            >
-              <PrimitivePanel primitive={primitive} resultsList={getResultsList(primitives, index)}>
-                {primitive?.children?.map((item, childIndex) => {
-                  return (
-                    <ConstructorItem
-                      key={item.id}
-                      id={item.id}
-                      listId={primitive.id}
-                      parentId={primitive.id}
-                      index={childIndex}
-                      justAdded={item.justAdded}
-                      nativeEvent={item.nativeEvent || undefined}
-                    >
-                      <PrimitivePanel
-                        parentId={primitive.id}
-                        primitive={item}
-                        resultsList={getResultsList(primitives, index)}
-                        parentHasSingleChild={groupData.hasSingleChild}
-                        noChangesForChildren={groupData.noChangesForChildren}
-                      />
-                    </ConstructorItem>
-                  );
-                })}
-              </PrimitivePanel>
-            </ConstructorItem>
-          );
-        })}
-      </div>
-
-      <div className="Constructor__tag Constructor__tag--close">&lt;/filter&gt;</div>
+      <div className="Constructor__tag">&lt;/filter&gt;</div>
     </section>
   );
 };
--- src/containers/Constructor/index.tsx
+++ src/containers/Constructor/index.tsx
@@ -1,9 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
-import Constructor from '../../components/Constructor';
+import { primitivesAttrs } from '../../data';
+
 import useSection from '../../hooks/useSection';
 import { RootState } from '../../store/store';
 import { moveToPlayground, purgePrimitives } from '../../store/primitivesSlice';
-import { isPrimitivesSection } from '../../store/types';
+import { isPrimitivesSection, PrimitiveItem } from '../../store/types';
+import { defaultSources } from '../../store/reducers/helpers/updateInPropInPrimitiveItem';
+
+import Constructor from '../../components/Constructor';
+import ConstructorPlaceholder from '../../components/atoms/ConstructorPlaceholder';
+import ConstructorItem from '../ConstructorItem';
+import PrimitivePanel from '../PrimitivePanel';
 
 const ConstructorContainer = () => {
   const { section } = useSection();
@@ -14,13 +21,58 @@ const ConstructorContainer = () => {
   const dispatch = useDispatch();
   if (!isPrimitivesSection(section)) return null;
 
+  const getResultsList = (primitives: PrimitiveItem[], index: number) => {
+    const results = primitives.slice(0, index).map((item) => item.id);
+    return Object.keys(defaultSources).concat(results);
+  };
+
   return (
     <Constructor
       section={section}
       primitives={primitives || undefined}
       purgePrimitives={() => dispatch(purgePrimitives({ section }))}
       moveToPlayground={() => dispatch(moveToPlayground({ section }))}
-    />
+    >
+      {!primitives?.length && <ConstructorPlaceholder section={section} />}
+      {primitives?.map((primitive: PrimitiveItem, index: number) => {
+        const groupData = primitivesAttrs[primitive.groupName];
+
+        return (
+          <ConstructorItem
+            key={primitive.id}
+            id={primitive.id}
+            listId="primitives"
+            index={index}
+            justAdded={primitive.justAdded}
+            nativeEvent={primitive.nativeEvent || undefined}
+          >
+            <PrimitivePanel primitive={primitive} resultsList={getResultsList(primitives, index)}>
+              {primitive?.children?.map((item, childIndex) => {
+                return (
+                  <ConstructorItem
+                    key={item.id}
+                    id={item.id}
+                    listId={primitive.id}
+                    parentId={primitive.id}
+                    index={childIndex}
+                    justAdded={item.justAdded}
+                    nativeEvent={item.nativeEvent || undefined}
+                  >
+                    <PrimitivePanel
+                      parentId={primitive.id}
+                      primitive={item}
+                      resultsList={getResultsList(primitives, index)}
+                      parentHasSingleChild={groupData.hasSingleChild}
+                      noChangesForChildren={groupData.noChangesForChildren}
+                    />
+                  </ConstructorItem>
+                );
+              })}
+            </PrimitivePanel>
+          </ConstructorItem>
+        );
+      })}
+    </Constructor>
   );
 };
 
