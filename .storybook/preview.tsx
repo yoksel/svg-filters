ab64de7c532859b2b567dbe92df60e0e5db9207c
yoksel <juliahtml@gmail.com>
2024-10-22 06:42:23 +0100

    Add router to storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      // to make NavLink work
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export default preview;
